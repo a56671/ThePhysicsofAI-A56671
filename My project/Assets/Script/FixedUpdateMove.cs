63a1bfd8ad7aa2e923dd59faf85b7f00e2ede9e7
PandinhaTV <57328243+PandinhaTV@users.noreply.github.com>
2026-02-04 16:35:17 +0000

    Time

---
+++ My project/Assets/Script/FixedUpdateMove.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedUpdateMove : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        this.transform.Translate(0f,0f,0.01f);
+    }
+}
---
+++ My project/Assets/Script/UpdateMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateMove : MonoBehaviour
+{
+
+    void Update()
+    {
+        this.transform.Translate(0f,0f,0.01f);
+    }
+}
